8ee1a33a0aa96d4cd7f61f459c54da32f65f43d2
Shubhendu <shubhendu.negi1990@gmail.com>
2020-11-19 14:43:23 +0530

    added homescreen

---
+++ frontend/src/products.js
@@ -0,0 +1,69 @@
+const products = [
+    {
+      _id: '1',
+      name: 'Chicken Banjara',
+      image: '/images/Chicken Banjara.jpg',
+      description:
+        'Tandoor Special',
+      brand: 'Masala Art',
+      category: 'Tandoor',
+      price: 180,
+      countInStock: 10,
+      rating: 4.5,
+      numReviews: 12,
+    },
+    {
+      _id: '2',
+      name: 'Chicken Tikka',
+      image: '/images/Chicken Tikka.jpg',
+      description:
+        'Spicy Tandoor Special',
+      brand: 'Masala Art',
+      category: 'Tandoor',
+      price: 180,
+      countInStock: 7,
+      rating: 4.0,
+      numReviews: 8,
+    },
+    {
+      _id: '3',
+      name: 'Dal Makhani',
+      image: '/images/Dal Makhani.jpg',
+      description:
+        'Bestseller',
+      brand: 'Masala Art',
+      category: 'Dals',
+      price: 160,
+      countInStock: 5,
+      rating: 5,
+      numReviews: 12,
+    },
+    {
+      _id: '4',
+      name: 'Green Peas Pulao',
+      image: '/images/Green Peas Pulao.jpg',
+      description:
+        'Veg Pulao Special',
+      brand: 'Masala Art',
+      category: 'Rice',
+      price: 160,
+      countInStock: 11,
+      rating: 4,
+      numReviews: 12,
+    },
+    {
+      _id: '5',
+      name: 'Veg Bhuna',
+      image: '/images/Veg Bhuna.jpg',
+      description:
+        'Bhuna Veg Special',
+      brand: 'Masala Art',
+      category: 'Veg Main Course',
+      price: 150,
+      countInStock: 7,
+      rating: 3.5,
+      numReviews: 10,
+    },
+  ]
+  
+  export default products
